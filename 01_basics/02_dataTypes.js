29d8cebadf7fb7b4aa0f64b2373363a40ec9a14d
Suraj Kumar Patel <90491429+suraj6268@users.noreply.github.com>
2024-03-19 03:55:19 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use Strict";       // treat all js code as newer version
+
+// alert(3+2)       // we are using nodeJs not browser
+
+console.log(3 
+    +
+     3)             // code run hoga lekin code reability low ho jayegi
+
+console.log("Suraj")
+
+
+let name = "Suraj"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined 
+//  symbol => unique
+
+// object
+
+
+
+console.log(typeof "Suraj")
+console.log(typeof age)
+
+console.log(typeof null)            // object
+console.log(typeof undefined)       // undefined
+
