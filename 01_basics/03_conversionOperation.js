f3a173c3fdce89bdbab637d51f9010da3944cd81
Suraj Kumar Patel <90491429+suraj6268@users.noreply.github.com>
2024-03-24 06:04:24 +0000

    operater precidance

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score)                       // string
-console.log(typeof(score))                      // string
+// console.log(typeof score)                       // string
+// console.log(typeof(score))                      // string
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)               // number
-console.log(valueInNumber)                      // NaN
+// console.log(typeof valueInNumber)               // number
+// console.log(valueInNumber)                      // NaN
 
 
 // "33" => 33
@@ -17,7 +17,7 @@ console.log(valueInNumber)                      // NaN
 let isLoggedIn = "suraj"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)                      // true
+// console.log(booleanIsLoggedIn)                      // true
 
 // 1 => true; 0 => false;
 // "" => false
@@ -28,6 +28,72 @@ console.log(booleanIsLoggedIn)                      // true
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)                           // 33
-console.log(typeof stringNumber)                    // string
+// console.log(stringNumber)                           // 33
+// console.log(typeof stringNumber)                    // string
+
+
+
+
+
+// ************************** Operation **********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)                            // -3
+
+
+// console.log(2+2)                                 // 4
+// console.log(2-2)                                 // 0
+// console.log(2*2)                                 // 4
+// console.log(2**3)                                // 8
+// console.log(2/3)                                 // 0.6666666666666666
+// console.log(2%3)                                 // 2
+
+
+
+
+let str1 = "Hello"
+let str2 = " Suraj"
+
+let str3 = str1+str2
+// console.log(str3)                                // Hello Suraj
+
+// console.log("1" + 2)                                // 12
+// console.log(1 + "2")                                // 12
+// console.log("1" + 2 + 2)                            // 122  
+// console.log(1 + 2 + "2")                            // 32     
+// console.log(1 + "2" + 2)                            // 122   
+
+// console.log(3 + 4 * 5 % 3)                          // 5
+// console.log(3 + 4 % 5 * 3)                          // 15
+
+
+// console.log(true)                                   // true
+// console.log(+true)                                  // 1
+
+
+// console.log(+false)                                 // 0
+
+
+// console.log(true+)      // ****** Not allowed *****
+
+
+// console.log(+"")                                    // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1)                                   // 4
+
+let gameCounter = 100
+++gameCounter
+gameCounter++
+console.log(gameCounter)                                // 102
+
+
+
+
+
 
