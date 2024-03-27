8d0e101a1e4ad4845dafd0371157fa2acc02de3d
Suraj Kumar Patel <90491429+suraj6268@users.noreply.github.com>
2024-03-27 03:29:37 +0000

    Summary of dataTypes

---
+++ datatypes-summary.js
@@ -0,0 +1,49 @@
+// Premitive
+
+// 7-types :- String, Number, Boolean, NULL, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outSideTemp = null
+
+let userEmail
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId)
+
+
+const BigNumber = 346820093194498319389n
+
+
+// Reference (Non premitive)
+
+// Array, Object, Function
+
+const heros = ["Saktiman", "naagraj", "doga"]
+
+let myObj = {
+    name : "Suarj",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+
+console.log(typeof anotherId)                              // symbol
+
+
+// console.log(typeof heros)                           // object
+
+// console.log(typeof myObj)                           // object
+
+// console.log(typeof myFunction)                      // function
+
